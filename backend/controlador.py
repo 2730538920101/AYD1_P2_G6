92c8574d988c487fa8d727e08639318cfa158e56
2730538920101 <2730538920101@ingenieria.usac.edu.gt>
2023-03-06 00:58:09 -0600

    [ADD] Se agrego el endpoint para crear usuarios con password encriptado.

--- backend/controlador.py
+++ backend/controlador.py
@@ -1,2 +1,12 @@
 from conexion import obtener_conexion
+from werkzeug.security import generate_password_hash
+
+def AgregarUsuario(nombre, apellido, correo, password):
+    conexion = obtener_conexion()
+    password_encriptado = generate_password_hash(password, "sha256", 30)
+    with conexion.cursor() as cursor:
+        cursor.execute("INSERT INTO USUARIO(NOMBRE, APELLIDO, CORREO, PASSWORD) VALUES (%s, %s, %s, %s)",
+                       (nombre, apellido, correo, password_encriptado))
+    conexion.commit()
+    conexion.close()
 
